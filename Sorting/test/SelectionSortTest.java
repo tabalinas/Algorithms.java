538cea26c87f5a370ec3ff6be8542ce751ffdbd1
Artem Tabalin <tabalinas@gmail.com>
2016-09-15 13:49:20 +0200

    Sorting: Correct Selection sort test name

--- Sorting/test/SelectionSortTest.java
+++ Sorting/test/SelectionSortTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class SelectionSortTest {
     
     @Test
-    public void testInsertionSort() {
+    public void testSelectionSort() {
         int[] array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
         int[] expectedResult = new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 };
 
