b39c2f7d604e03195c2e821e6aec5b436032d0e9
Artem Tabalin <tabalinas@gmail.com>
2016-09-15 14:08:41 +0200

    Sorting: Refactor naming for Merge sort

--- Sorting/src/MergeSort.java
+++ Sorting/src/MergeSort.java
@@ -12,24 +12,28 @@ public class MergeSort {
         if(start >= end)
             return;
         
-        int middle = (start + end) / 2 + 1;
+        int middle = (end - start) / 2 + start + 1;
         
         doSort(arr, start, middle - 1);
         doSort(arr, middle, end);
         
+        merge(arr, start, middle, end);
+    }
+
+    private static void merge(int[] arr, int start, int middle, int end) {
         int[] leftPart = Arrays.copyOfRange(arr, start, middle);
-        int left = 0;
-        int right = middle;
-        int index = start;
+        int leftIndex = 0;
+        int rightIndex = middle;
+        int resultIndex = start;
                 
-        while(left < leftPart.length && right <= end) {
-            arr[index++] = (leftPart[left] > arr[right]) 
-                    ? arr[right++] 
-                    : leftPart[left++];
+        while(leftIndex < leftPart.length && rightIndex <= end) {
+            arr[resultIndex++] = (leftPart[leftIndex] > arr[rightIndex])
+                    ? arr[rightIndex++] 
+                    : leftPart[leftIndex++];
         }
         
-        while(left < leftPart.length) {
-            arr[index++] = leftPart[left++];
+        while(leftIndex < leftPart.length) {
+            arr[resultIndex++] = leftPart[leftIndex++];
         }
     }
 
